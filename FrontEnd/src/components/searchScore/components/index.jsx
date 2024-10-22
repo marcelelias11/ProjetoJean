c9f6749938ecc48b80606478d7aa5e5f439faffa
Marcelelias11 <marcelelias11@gmail.com>
2024-10-22 16:04:16 -0300

    Comentários e implementação do back

--- FrontEnd/src/components/searchScore/components/index.jsx
+++ FrontEnd/src/components/searchScore/components/index.jsx
@@ -1,14 +1,14 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"; //Cria as rotas pelo react-router-dom
 //import Home from '../components/home/home'
 //import Search from "./searchScore/searchLayout/searchLayout";
-import SearchCourse from "./searchCourse/searchCourse";
-import SearchCampus from "./searchCampus/searchCampus";
-import SearchTurn from "./searchTurn/searchTurn";
-import SearchType from "./searchType/searchType";
+import SearchCourse from "./searchCourse/searchCourse"; //Importa as páginas utilizadas
+import SearchCampus from "./searchCampus/searchCampus"; //Importa as páginas utilizadas
+import SearchTurn from "./searchTurn/searchTurn"; //Importa as páginas utilizadas
+import SearchType from "./searchType/searchType"; //Importa as páginas utilizadas
 
 function SearchScore() {
 
-  return (
+  return ( //Função que cria as rotas que serão utilizadas
     <>
         <Routes>
           <Route path="/curso" element={<SearchCourse/>}/>
